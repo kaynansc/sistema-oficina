b0265143e620e520de7df59211435aad16602d01
Eduardo Silva <eduuhrafael@gmail.com>
2020-07-30 20:50:09 -0300

    Ajustes na Master e Tela de Login

--- main.js
+++ main.js
@@ -6,14 +6,17 @@ function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 600,    
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+  
+  mainWindow.setMenuBarVisibility(false);
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // mainWindow.loadFile('index.html')
+  mainWindow.loadFile('app/master.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
